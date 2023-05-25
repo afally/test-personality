3b22fdedaac4433c6e9d19a3790e24b399679c7b
ayofalo <ayoflw@gmail.com>
2023-05-25 22:41:42 +0100

    tests updated 10

--- packages/express-backend/config/keys.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-    mongoURI: MONGO_URI = "mongodb+srv://ayoflm:System2023@cluster0.jkgo7ny.mongodb.net/?retryWrites=true&w=majority",                    
-    secretOrKey: "secret"
-};
